75c3fa21fcd830a3a45fb32f4be3e46af798cc48
shahidhmohamed <mohamedshahidh81@gmail.com>
2024-12-01 02:04:45 +0530

    b4_changes the file names

--- lib/routing/app_routes.dart
+++
@@ -1,9 +0,0 @@
-class AppRoute {
-  static const String SPLASH = '/';
-  static const String HOME = '/home';
-  static const String SORT_NOTE = '/sort';
-  static const String NEWS_SOURCES = '/sources';
-  static const String FAV = '/fav';
-  static const String SAVED_NEWS = '/saved-news';
-
-}
